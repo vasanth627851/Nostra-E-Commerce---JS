4e10ed68c31099f5171052dac63a9e7aca6067e9
vasanth627851 <vasanths627851@gmail.com>
2025-02-16 18:55:30 +0530

    Nostra project

---
+++ index.js
@@ -0,0 +1,56 @@
+// creating closing top navbar
+
+var topsale = document.getElementById("topsale")
+var closingX = document.getElementById("offer-close")
+
+
+closingX.addEventListener("click", function () {
+    topsale.style.display = "none"
+})
+
+//Slider Image Section 
+
+var sliderleftbutton = document.getElementById("slider-left-activate")
+var sliderrightbutton =document.getElementById("slider-right-activate")
+var sliderimage= document.querySelector(".slider-image-container")
+var slidermargin=0
+
+sliderrightbutton.addEventListener("click",
+
+function(){
+    slidermargin=slidermargin+100
+
+    if(slidermargin>200)
+    {
+        slidermargin=0
+        sliderimage.style.marginLeft=0;
+    }
+    else{
+        sliderimage.style.marginLeft="-"+slidermargin+"vw";
+    }
+    
+   
+
+}
+)
+
+sliderleftbutton.addEventListener("click",
+
+function(){
+
+    if(slidermargin==0)
+    {
+       
+        slidermargin=200
+        sliderimage.style.marginLeft="-"+slidermargin+"vw";
+    }
+    else{
+
+        slidermargin=slidermargin-100
+    sliderimage.style.marginLeft="-"+slidermargin+"vw";
+    }
+    
+    
+}
+)
+
